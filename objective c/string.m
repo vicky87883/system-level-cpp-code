84279f3ec9dcee49ad3cd1c9c467dee99b08de71
Vikram <vikram>
2025-07-08 07:27:36 +0530

    commit updated

---
+++ objective c/string.m
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+int main(int argc,const char * argv[])
+{
+    @autoreleasepool{
+        NSString *city = @"New York";
+        NSLog(@"City:%@",city);
+    }
+    return 0;
+}
